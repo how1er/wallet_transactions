ddf73734bb39909f620c28fe12cc85a9fe312571
how1er <azamatkfu@gmail.com>
2022-10-08 18:05:52 +0300

    add Serializer and wallet_list view for Wallet

---
+++ test_project/wallets/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import Wallet
+
+
+class WalletSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Wallet
+        fields = ['id', 'name', 'type', 'currency', 'balance', 'created_on', 'modified_on']
